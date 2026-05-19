84da39631f1bbe3d54fab8256803bd0308f593f3
Alex Tsarkov <alexandrtsarkov410@gmail.com>
2026-05-19 18:14:52 +0300

    Changed progect3

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
 
 class Phone(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -9,3 +9,7 @@ class Phone(models.Model):
     release_date = models.DateField()
     lte_exists = models.BooleanField()
     slug = models.SlugField(max_length=200, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super(Phone, self).save(*args, **kwargs)
